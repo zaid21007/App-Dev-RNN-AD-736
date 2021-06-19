2d454184189920c35d9e97a5dfa51e1a920a9163
zaid21007 <77574747+zaid21007@users.noreply.github.com>
2021-06-19 23:05:59 +0500

    Delete init directory

--- init/config.js
+++
@@ -1,7 +0,0 @@
-const config = require('config');
-module.exports = function () {
-  if (!config.get('jwtKey'))
-    throw new Error(
-      'Enviornment Variable Error: Enviornment Variables not Set'
-    );
-};
--- init/getRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const users = require('../routes/users');
-
-module.exports = function (app) {
-  app.use(cors());
-  app.use(express.json());
-  app.use('/api/users', users);
-};
